6bea691ce5d8228831973b8a2acc52d635072d8a
CoderLambert <l156486648@163.com>
2023-01-06 21:53:50 +0800

    组件库初始化

---
+++ app.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h1>welcome sdasd</h1>
+  </div>
+  <AppAlert>
+    sdasd
+  </AppAlert>
+</template>
---
+++ components/AppAlert.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <h1>welcome to be homepage</h1>
+    </div>
+
+    <span>
+     <slot />
+    </span>
+</template>
